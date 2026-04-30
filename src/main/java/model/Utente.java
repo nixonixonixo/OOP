62bc3e7b135d7f3d89f028bfd6280522c5bb6e9b
nixonixonixo <n.ragozzini@studenti.unina.it>
2026-04-30 10:04:35 +0200

    parte 1 utente

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -2,4 +2,20 @@ package model;
 
 public class Utente {
 
+    //costruttore Utente
+    public Utente(int idUtente, String nome, String cognome, String email){
+        this.idUtente = idUtente;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+    }
+
+    //attributi Utente
+    protected int idUtente;
+    protected String nome;
+    protected String cognome;
+    protected String email;
+
+    //metodi Utente
+
 }
