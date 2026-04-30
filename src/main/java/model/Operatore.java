86fba0a2bdb987a7a877603e225606ceb19da992
RussoGaetano06 <russogaetano01@gmail.com>
2026-04-30 10:24:07 +0200

    aggiungo la classe Operatore.java

--- src/main/java/model/Operatore.java
+++ src/main/java/model/Operatore.java
@@ -1,4 +1,24 @@
 package model;
 
-public class Operatore {
+public class Operatore  extends Utente{
+
+    public Operatore(int idUtente, String nome,String cognome, String email,Ruolo ruolo){
+        super(idUtente,nome,cognome,email);
+        this.ruolo=ruolo;
+    }
+
+    private  Ruolo ruolo;
+
+    public enum Ruolo{
+        ADMIN,
+        ADDETTONOLEGGIO,
+        MANUTENTORE
+
+    }
+
+    //metodi Operatore
+    @Override
+    public String toString(){
+        return super.toString() + " " + ruolo;
+    }
 }
