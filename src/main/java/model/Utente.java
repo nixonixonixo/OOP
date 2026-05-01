c377e33c1f561782f7c3b9accf6b5e5d6b978f7b
nixonixonixo <n.ragozzini@studenti.unina.it>
2026-05-01 12:55:13 +0200

    aggiunta password, hashing e getter

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -1,9 +1,21 @@
 package model;
 
+//import per creazione password sicura
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 public class Utente {
 
     //costruttore Utente
-    public Utente(int idUtente, String nome, String cognome, String email){
+    public Utente(int idUtente, String username, String password, String nome, String cognome, String email){
+        if (username == null || username.isBlank()) {
+            throw new IllegalArgumentException("Username non valido");
+        }
+        if (password == null || password.isBlank()) {
+            throw new IllegalArgumentException("Password non valida");
+        }
         if (nome == null || nome.isBlank()) {
             throw new IllegalArgumentException("Nome non valido");
         }
@@ -14,18 +26,53 @@ public class Utente {
             throw new IllegalArgumentException("Email non valida");
         }
         this.idUtente = idUtente;
+        this.username = username;
+        this.passwordHash = createHash(password);
         this.nome = nome;
         this.cognome = cognome;
         this.email = email;
     }
 
     //attributi Utente
-    protected int idUtente;
-    protected String nome;
-    protected String cognome;
-    protected String email;
+    private int idUtente;
+    private String username;
+    private String passwordHash;
+    private String nome;
+    private String cognome;
+    private String email;
 
     //metodi Utente
+    //metodo per la crittografia della password
+    private String createHash(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(hashBytes);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Algoritmo di hashing non trovato", e);
+        }
+    }
+
+    public int getIdUtente(){
+        return idUtente;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
     @Override
     public String toString() {
         return idUtente + " " + nome + " " + cognome  + " " + email;
